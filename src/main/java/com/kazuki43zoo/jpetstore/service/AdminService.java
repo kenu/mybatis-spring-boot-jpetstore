495444dbe7b8a7f9467eb6aa65bd780c60bc22d2
d36choi <yuio1126@gmail.com>
2021-02-05 00:36:24 +0900

    feat: Add product mapper

--- src/main/java/com/kazuki43zoo/jpetstore/service/AdminService.java
+++ src/main/java/com/kazuki43zoo/jpetstore/service/AdminService.java
@@ -1,7 +1,9 @@
 package com.kazuki43zoo.jpetstore.service;
 
 import com.kazuki43zoo.jpetstore.domain.Account;
+import com.kazuki43zoo.jpetstore.domain.Product;
 import com.kazuki43zoo.jpetstore.mapper.AdminMapper;
+import com.kazuki43zoo.jpetstore.mapper.ProductMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ import java.util.stream.Stream;
 public class AdminService {
 
     private final AdminMapper adminMapper;
+    private final ProductMapper productMapper;
 
     public int getAccountCount() {
         return adminMapper.getAccountCount();
@@ -24,6 +27,17 @@ public class AdminService {
         return Stream.of(Optional.ofNullable(keywords).orElse("").split("\\s+"))
                 .distinct()
                 .flatMap(x -> adminMapper.getAccountList(x).stream())
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public int getProductCount() { return productMapper.getProductCount(); }
+
+
+    public List<Product> searchProductList(String keywords) {
+        return Stream.of(Optional.ofNullable(keywords).orElse("").split("\\s+"))
+                .distinct()
+                .flatMap(x -> productMapper.searchProductList(x).stream())
                 .distinct()
                 .collect(Collectors.toList());
     }
